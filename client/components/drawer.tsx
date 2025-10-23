49b67913d823d2c71ec950a3c6f458a7c3b6fd5d
keaneph <nearpharelle@gmail.com>
2025-10-23 15:55:41 +0800

    feat/contact forms fully functional

--- client/components/drawer.tsx
+++ client/components/drawer.tsx
@@ -1,17 +1,18 @@
 "use client"
 
 import * as React from "react"
-
+import Image from "next/image";
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { Label } from "@/components/ui/label"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
 import {
   Drawer,
   DrawerClose,
   DrawerContent,
-  DrawerDescription,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
@@ -26,42 +27,43 @@ import {
   FieldLegend,
   FieldSeparator,
   FieldSet,
-  FieldTitle,
 } from "@/components/ui/field"
+
+import celebrating from "@/public/celebrating.png"
+import eureka from "@/public/eureka.png"
+import mochi from "@/public/mochi.png"
+import { contactFormSchema } from "@/lib/schemas"
+import { send } from "@/lib/email"
 
 interface ContactDrawerProps {
   trigger: React.ReactNode
 }
 
 export function ContactDrawer({ trigger }: ContactDrawerProps ) {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
+
+  const form = useForm<z.infer<typeof contactFormSchema>>({
+    resolver: zodResolver(contactFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
   })
 
   const [submitted, setSubmitted] = useState(false)
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }))
-  }
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    console.log("Form submitted:", formData)
+  function onSubmit(data: z.infer<typeof contactFormSchema>) {
     setSubmitted(true)
+    console.log(data)
+    form.reset()
+    send(data)
 
-    // reset form after 5 seconds
+    // reset after 5 seconds
     setTimeout(() => {
-      setFormData({ name: "", email: "", subject: "", message: "" })
       setSubmitted(false)
-    }, 5000)
-  }
+    }, 7000)
+}
 
   return (
     <Drawer>
@@ -70,8 +72,7 @@ export function ContactDrawer({ trigger }: ContactDrawerProps ) {
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle className="text-xl sr-only">Get in Touch</DrawerTitle>
-          <DrawerDescription className="sr-only">We&apos;ll get back to you as soon as possible.</DrawerDescription>
+          <DrawerTitle> </DrawerTitle>
         </DrawerHeader>
 
         <div className="px-4 pb-4 flex justify-center">
@@ -80,110 +81,147 @@ export function ContactDrawer({ trigger }: ContactDrawerProps ) {
               <div className="text-center">
                 <div className="text-2xl font-semibold text-green-600 mb-2">✓ Message Sent!</div>
                 <p className="text-muted-foreground">Thank you for reaching out. We&apos;ll be in touch soon.</p>
+                <Image src={celebrating} alt="Message Sent" className="mx-auto w-66" />
               </div>
             </div>
-          ) : (
-            <form onSubmit={handleSubmit} className="space-y-4 w-full max-w-xl">
-              <FieldSet>
-                <FieldLegend className="text-center">Get in Touch</FieldLegend>
-                <FieldDescription className="text-center">
-                  Have questions or feedback? Fill out the form below to contact our support team.
-                </FieldDescription>
-                <FieldSeparator />
-
-                <FieldGroup>
-                  {/* name */}
-                  <Field orientation="responsive">
-                    <FieldContent>
-                      <FieldLabel htmlFor="firstName">Name</FieldLabel>
-                      <FieldDescription>
-                        Enter name or organization
+          ) : (    
+            <div className="relative flex flex-col items-center justify-center">
+                <Image src={mochi} alt="Mochi Thinking" className="absolute cursor-pointer -bottom-10 right-160 w-40 transition-transform hover:scale-105" />
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full max-w-xl">
+                  <FieldSet>
+                      <FieldLegend className="text-center font-bold text-main !text-xl">Get in Touch</FieldLegend>
+                      <FieldDescription className="text-center ">
+                          Have questions or feedback? Fill out the form below to contact our support team.
                       </FieldDescription>
-                    </FieldContent>
-                    <Input
-                      id="Name"
-                      name="Name"
-                      placeholder="MOGC"
-                      value={formData.name}
-                      onChange={handleChange}
-                      required
-                    />
-                  </Field>
+                      <FieldSeparator />
+                      <FieldGroup>
+                          <Controller
+                              name="name"
+                              control={form.control}
+                              render={({ field, fieldState }) => (
+                              <Field data-invalid={fieldState.invalid} orientation="responsive">
+                                  <FieldContent>
+                                      <FieldLabel htmlFor="firstName">
+                                          Name
+                                      </FieldLabel>
+                                      <FieldDescription>
+                                          Enter name or organization
+                                      </FieldDescription>
+                                  </FieldContent>
+                                  <div className="flex flex-col w-full">
+                                    <Input
+                                        {...field}
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="MOGC"
+                                        autoComplete="off"
+                                        className="min-w-[250px]"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                  </div>
+                              </Field>
+                              )}
+                          />
+                          <Controller
+                              name="email"
+                              control={form.control}
+                              render={({ field, fieldState }) => (
+                                  <Field data-invalid={fieldState.invalid} orientation="responsive">
+                                      <FieldContent>
+                                          <FieldLabel htmlFor="email">
+                                              Email
+                                          </FieldLabel>
+                                          <FieldDescription>
+                                              We&apos;ll use this to get back to you.
+                                          </FieldDescription>
+                                      </FieldContent>
+                                      <div className="flex flex-col w-full">
+                                        <Input
+                                            {...field}
+                                            id="email"
+                                            aria-invalid={fieldState.invalid}
+                                            placeholder="email@example.com"
+                                            className="min-w-[250px]"
+                                        />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} />
+                                        )}
+                                      </div>
+                                  </Field>
+                              )}
+                          />
+                          <Controller
+                              name="subject"
+                              control={form.control}
+                              render={({ field, fieldState }) => (
+                                  <Field data-invalid={fieldState.invalid} orientation="responsive">
+                                      <FieldContent>
+                                          <FieldLabel htmlFor="subject">
+                                              Subject
+                                          </FieldLabel>
+                                          <FieldDescription>
+                                              What is this message about?
+                                          </FieldDescription>
+                                      </FieldContent>
+                                      <div className="flex flex-col w-full">
+                                        <Input
+                                            {...field}
+                                            id="subject"
+                                            aria-invalid={fieldState.invalid}
+                                            placeholder="Bug report"
+                                            className="min-w-[250px]"
+                                        />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} />
+                                        )}
+                                      </div>
+                                  </Field>
+                              )}
+                          />
+                          <Controller
+                              name="message"
+                              control={form.control}
+                              render={({ field, fieldState }) => (
+                                  <Field data-invalid={fieldState.invalid} orientation="responsive">
+                                      <FieldContent>
+                                      <FieldLabel htmlFor="message">Message</FieldLabel>
+                                      <FieldDescription>
+                                          Describe your concern or feedback in detail.
+                                      </FieldDescription>
+                                      </FieldContent>
+                                      <div className="flex flex-col w-full">
+                                        <Textarea
+                                            {...field}
+                                            id="message"
+                                            aria-invalid={fieldState.invalid}
+                                            placeholder="Your message here..."
+                                            className="max-h-[50px] resize-none min-w-[300px]"
+                                        />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} />
+                                        )}
+                                      </div>
+                                  </Field>
+                              )}
+                          />
 
-                  {/* email */}
-                  <Field orientation="responsive">
-                    <FieldContent>
-                      <FieldLabel htmlFor="email">Email</FieldLabel>
-                      <FieldDescription>
-                        We’ll use this to get back to you.
-                      </FieldDescription>
-                    </FieldContent>
-                    <Input
-                      id="email"
-                      name="email"
-                      type="email"
-                      placeholder="email@example.com"
-                      value={formData.email}
-                      onChange={handleChange}
-                      required
-                    />
-                  </Field>
-
-                  {/* subject */}
-                  <Field orientation="responsive">
-                    <FieldContent>
-                      <FieldLabel htmlFor="subject">Subject</FieldLabel>
-                      <FieldDescription>
-                        What is this message about?
-                      </FieldDescription>
-                    </FieldContent>
-                    <Input
-                      id="subject"
-                      name="subject"
-                      placeholder="Bug report"
-                      value={formData.subject}
-                      onChange={handleChange}
-                      required
-                    />
-                  </Field>
-
-                  {/* message */}
-                  <Field orientation="responsive">
-                    <FieldContent>
-                      <FieldLabel htmlFor="message">Message</FieldLabel>
-                      <FieldDescription>
-                        Describe your concern or feedback in detail.
-                      </FieldDescription>
-                    </FieldContent>
-                    <Textarea
-                      id="message"
-                      name="message"
-                      placeholder="Your message here..."
-                      value={formData.message}
-                      onChange={handleChange}
-                      className="max-h-[50px] resize-none sm:max-w-[300px]"
-                      required
-                    />
-                  </Field>
-
-                  {/* buttons */}
-                  <Field orientation="responsive" className="flex flex-wrap">
-                    <Button
-                      type="submit"
-                      className="cursor-pointer flex-1 bg-main hover:bg-main/90"
-                    >
-                      Send Message
-                    </Button>
-                    <DrawerClose asChild>
-                      <Button variant="outline" className="cursor-pointer flex-1 bg-transparent">
-                        Cancel
-                      </Button>
-                    </DrawerClose>
-                  </Field>
-                </FieldGroup>
-              </FieldSet>
-            </form>
-          )}
+                          <Field orientation="responsive" className="flex flex-wrap">
+                              <Button type="submit" className="cursor-pointer flex-1 bg-main hover:bg-main/90">
+                                  Send Message
+                              </Button>
+                              <DrawerClose asChild>
+                              <Button variant="outline" className="cursor-pointer flex-1 bg-transparent">
+                                  Cancel
+                              </Button>
+                              </DrawerClose>
+                          </Field>
+                      </FieldGroup>
+                  </FieldSet>
+              </form>
+              <Image src={eureka} alt="Thinking" className="absolute -bottom-15 left-160 w-55 transition-transform hover:scale-105 cursor-pointer" />
+            </div>
+          )}   
         </div>
       </DrawerContent>
     </Drawer>
---
+++ client/lib/schemas.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export const contactFormSchema = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters"),
+  email: z.string().email("Invalid email address"),
+  subject: z.string().min(2, "Subject must be at least 2 characters"),
+  message: z.string().min(2, "Message must be at least 2 characters"),
+})
