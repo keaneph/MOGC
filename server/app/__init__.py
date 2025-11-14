cfe5b881925070184e22453df9d488fd84c95955
keaneph <nearpharelle@gmail.com>
2025-11-14 13:46:57 +0800

    fix/getting ready for merging with main

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -14,7 +14,6 @@ CORS(
 		"http://localhost:3000",
 		"https://mogc.vercel.app",
 		"https://mogc.onrender.com",
-        "https://mogc-git-bugfix-mogc-76-verc-bee40a-nearpharelle-7322s-projects.vercel.app",
 	],
 	supports_credentials=True,
 )
